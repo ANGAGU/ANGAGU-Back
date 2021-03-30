bc650adff2ad6a50a423d64b6dde2f8a29287d99
안현준 <hja5432@ajou.ac.kr>
2021-03-30 16:17:00 +0900

    migration to typescript

--- app.ts
+++ app.ts
@@ -1,22 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express, { Request, Response,NextFunction } from 'express';
+import path from 'path';
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+import createError from 'http-errors';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app: express.Application = express();
 
-var app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -28,7 +25,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err:any, req:Request, res:Response, next:NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +35,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
