81016bed8ea688f0d56b2ac7f0b2aa9202be80c4
안현준 <hja5432@ajou.ac.kr>
2021-04-05 01:20:39 +0900

    set up eslint

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response,NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
+import createError from 'http-errors';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
-import createError from 'http-errors';
+import customerRouter from './routes/customer';
 
 const app: express.Application = express();
-
 
 // view engine setup
 
@@ -18,14 +18,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/customer', customerRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err:any, req:Request, res:Response, next:NextFunction) {
+app.use((err:any, req:Request, res:Response, next:NextFunction) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.ts
+++ routes/users.ts
@@ -1,8 +1,9 @@
-import * as express from 'express';
+import express from 'express';
+
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
