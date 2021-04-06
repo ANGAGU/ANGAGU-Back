d445e26eb4cd14368ff731c66071fa4423979db6
안현준 <hja5432@ajou.ac.kr>
2021-04-06 23:09:57 +0900

    fix: customer login #3.2

--- controller/customer.controller.ts
+++ controller/customer.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import config from '../config.json';
 import { getCustomerByEmailPassword } from '../database/customer-service';
 import { jwtSignUser, User, isEmail } from './utils';
 
@@ -14,26 +16,36 @@ const login = async (req:Request, res:Response):Promise<void> => {
       });
       return;
     }
-
     const result = await getCustomerByEmailPassword(req.body.email, req.body.password);
     if (result.status === 'success') {
-      const user:User = result.data[0];
-      user.type = 'customer';
-      const token = jwtSignUser(user);
-      res.json({
-        status: 'success',
-        data: {
-          user,
-          token,
-        },
-      });
+      if (result.data.length === 1) {
+        const user:User = result.data[0];
+        user.type = 'customer';
+        const token = jwtSignUser(user);
+        res.json({
+          status: 'success',
+          data: {
+            user,
+            token,
+          },
+        });
+      } else {
+        res.status(202).json({
+          status: 'error',
+          data: {
+            errCode: 102,
+          },
+          message: 'can not find user',
+        });
+      }
     } else {
-      res.status(202).json({
+      res.status(500).json({
         status: 'error',
         data: {
-          errCode: 102,
+          errCode: 100,
+          data: result.data,
         },
-        message: 'can not find user',
+        message: 'db select error',
       });
     }
   } catch (err) {
