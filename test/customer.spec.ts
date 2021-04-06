b680e55c4af6334c96573f29ebe64a22219a6d03
안현준 <hja5432@ajou.ac.kr>
2021-04-06 23:07:59 +0900

    feat: add customer test case #3.2

--- test/customer.spec.ts
+++ test/customer.spec.ts
@@ -14,5 +14,8 @@ describe('customer test', () => {
   it('login test', async () => {
     const res = await chai.request(server).post('/customer/login').send({ email: 'abcd', password: 'abcd' });
     expect(res.status).to.equal(202);
+
+    const res2 = await chai.request(server).post('/customer/login').send({ email: 'abcd@acd.com', password: 'abcdadf' });
+    expect(res2.status).to.equal(202);
   });
 });
