02dac04dd7ce879bcc34142c87bf2096fde93814
nnfsth <hja5432@gmail.com>
2021-05-14 15:43:02 +0900

    fix: db password col

--- SQL/angagu.sql
+++ SQL/angagu.sql
@@ -54,7 +54,7 @@ DROP TABLE IF EXISTS `admin`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admin` (
   `id` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -188,7 +188,7 @@ DROP TABLE IF EXISTS `company`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `company` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `password` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `name` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `phone_number` varchar(45) NOT NULL,
@@ -227,7 +227,7 @@ DROP TABLE IF EXISTS `customer`;
 CREATE TABLE `customer` (
   `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `name` varchar(45) NOT NULL,
   `birth` date NOT NULL,
   `phone_number` varchar(45) NOT NULL,
